0dccdd762697a7d86cc8bded18c74ced6ab2167f
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2023-02-21 11:44:06 -0500

    Fresh guided tour

---
+++ routes/greet/[name].tsx
@@ -0,0 +1,12 @@
+
+import { PageProps } from "$fresh/server.ts";
+
+export default function GreetPage(props: PageProps) {
+  const { name } = props.params;
+
+  return (
+    <main>
+      <p>Greetings to you, {name}!</p>
+    </main>
+  );
+}
